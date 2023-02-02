607bb1733eccaeeeaaea7bce96575abeefcf974f
dilaraozkoc <dilaraozkoc26@gmail.com>
2023-02-02 16:00:03 +0300

    Ai
    Ai find each other

--- Sumo.io/Assets/GameFolder/Scripts/Concrete/Ai.cs
+++ Sumo.io/Assets/GameFolder/Scripts/Concrete/Ai.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Ai : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		rb = GetComponentInParent<Rigidbody>();
+
+	}
+	private void Update()
+	{
+		MoveClosestEnemy();
+	}
+
+	private void MoveClosestEnemy()
+	{
+		transform.DOKill();
+		transform.DOMove(GetClosestEnemy().position, GetTweenTime(GetClosestEnemy().position));
+	}
+	public Transform GetClosestEnemy()
+	{
+		Transform minTransform = null;
+		float minDistance = Mathf.Infinity;
+		Transform currentTransform = transform;
+
+		foreach (var item in AiManager.Instance.enemies)
+		{
+			if (transform.position != item.transform.position)
+			{
+				float distance = Vector3.Distance(item.transform.position, currentTransform.position);
+				if (distance < minDistance)
+				{
+					minTransform = item.transform;
+					minDistance = distance;
+					transform.LookAt(minTransform);
+					
+				}
+			}
+		}
+		return minTransform;
+	}
+
+	public float GetTweenTime(Vector3 targetPos)
+	{
+		return Vector3.Distance(transform.position, targetPos) / 1f;
+	}
 }
--- Sumo.io/Assets/GameFolder/Scripts/Concrete/AiManager.cs
+++ Sumo.io/Assets/GameFolder/Scripts/Concrete/AiManager.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class AiManager : MonoBehaviour
 {
+	public static AiManager Instance;
 	public GameObject enemyPrefab;
 
-	[SerializeField] private List<GameObject> enemies;
+	[SerializeField] public List<GameObject> enemies;
 
 	[SerializeField] private int enemyNumber;
 
+	private void Awake()
+	{
+		Instance = this;
+	}
 	private void Start()
 	{
 		SpawnEnemies();
@@ -18,7 +23,7 @@ public class AiManager : MonoBehaviour
 	{
 		for (int i = 0; i < enemyNumber; i++)
 		{
-			int angle = (360 / enemyNumber) * i;
+			float angle = /*Random.value * 360*/(360 / enemyNumber) * i;
 			GameObject enemyGO = Instantiate(enemyPrefab, PlaceEnemyArounCircle(Vector3.zero,5f,angle),Quaternion.identity,transform);
 			enemies.Add(enemyGO);
 		}
@@ -34,4 +39,6 @@ public class AiManager : MonoBehaviour
 		position.y = center.y;
 		return position;
 	}
+
+	
 }
