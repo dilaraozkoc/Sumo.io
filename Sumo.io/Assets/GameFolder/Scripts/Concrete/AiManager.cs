8734f5c96c8a87196de5332102df7ae1da019e16
dilaraozkoc <dilaraozkoc26@gmail.com>
2023-02-02 14:03:04 +0300

    Ai Manager
    Ai manager, spawn ai's and place around a player as a circle

--- Sumo.io/Assets/GameFolder/Scripts/Concrete/AiManager.cs
+++ Sumo.io/Assets/GameFolder/Scripts/Concrete/AiManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class AiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject enemyPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private List<GameObject> enemies;
+
+	[SerializeField] private int enemyNumber;
+
+	private void Start()
+	{
+		SpawnEnemies();
+	}
+	private void SpawnEnemies()
+	{
+		for (int i = 0; i < enemyNumber; i++)
+		{
+			int angle = (360 / enemyNumber) * i;
+			GameObject enemyGO = Instantiate(enemyPrefab, PlaceEnemyArounCircle(Vector3.zero,5f,angle),Quaternion.identity,transform);
+			enemies.Add(enemyGO);
+		}
+		
+	}
+
+	private Vector3 PlaceEnemyArounCircle(Vector3 center, float radius, float _angle)
+	{
+		float angle = _angle;
+		Vector3 position;
+		position.x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+		position.z = center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+		position.y = center.y;
+		return position;
+	}
 }
